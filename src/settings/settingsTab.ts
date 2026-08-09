67ccb740643356c5d0f606d3417402aa5b60eda3
threeyang3 <2499436961@qq.com>
2026-08-09 20:43:26 +0800

    fix: update template guide link

--- src/settings/settingsTab.ts
+++ src/settings/settingsTab.ts
@@ -128,7 +128,7 @@ export class BangumiSettingTab extends PluginSettingTab {
 
 		helpLinksDiv.createEl('button', { text: tn('settings', 'templateGuide'), cls: 'mod-cta' }, btn => {
 			btn.addEventListener('click', () => {
-				this.openExternalLink('https://github.com/threeyang3/bangumi-sync/blob/main/docs/TEMPLATE_GUIDE.md');
+				this.openExternalLink('https://github.com/threeyang3/bangumi-sync/blob/main/docs/user/TEMPLATE_GUIDE.md');
 			});
 		});
 
